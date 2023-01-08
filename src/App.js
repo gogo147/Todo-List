2a203544e2986fb95c8d56f08860fe093409c9dd
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2023-01-08 01:37:17 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header.js';
+import Form from './components/Form';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
-    <div className="App">
-     
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form 
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        </div>
+      </div>
     </div>
   );
 }
