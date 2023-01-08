7a3b9eea56470111e68c4a6f434beff1f282e89c
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2023-01-08 02:59:26 +0100

    completed todolist

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Form from './components/Form';
 import TodosList from './components/TodosList';
 
 function App() {
+
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+
   return (
     <div className='container'>
       <div className='app-wrapper'>
@@ -19,10 +22,17 @@ function App() {
             setInput={setInput}
             todos={todos}
             setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
           />
         </div>
         <div>
-          <TodosList todos={todos} setTodos={setTodos} />
+          <TodosList 
+            todos={todos} 
+            setTodos={setTodos} 
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
         </div>
       </div>
     </div>
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,16 +1,46 @@
 import React from 'react'
 
-const TodosList = ({ todos, setTodos }) => {
+const TodosList = ({ todos, setTodos, setEditTodo }) => {
+    const  handleComplete = (todo) => {
+        setTodos(
+            todos.map((item) => {
+                if(item.id === todo.id) {
+                    return { ...item, completed: !item.completed};
+                }
+                return item;
+            })
+        )
+    }
+
+    const handleEdit = ({id}) => {
+        const findTodo = todos.find((todo) => todo.id === id);
+        setEditTodo(findTodo); 
+    }
+
+    const handleDelete = ({id}) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
   return (
     <div>
         {todos.map((todo) => (
-            <li className='todo-list' key={todo.id}>
+            <li className='list-item' key={todo.id}>
                 <input 
                     type='text' 
                     value={todo.title} 
-                    className='list' 
+                    className={`list ${todo.completed ? 'complete' : ''}`}
                     onChange={(event) => event.preventDefault()}
                 />
+                <div>
+                    <button className='button-complete task-button' onClick={() => handleComplete(todo)}>
+                        a
+                    </button>
+                    <button className='button-edit task-button' onClick={() => handleEdit(todo)}>
+                        b
+                    </button>
+                    <button className='button-delete task-button' onClick={() => handleDelete(todo)}>
+                        c
+                    </button>
+                </div>
             </li>
         ))}
     </div>
